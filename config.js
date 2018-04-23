03a084be401bdbb585a520db75a63e41d9a26fbb
maogefff <806593449@qq.com>
2018-04-23 17:13:11 +0800

    update v1

---
+++ config.js
@@ -0,0 +1,7 @@
+// 配置
+websocketserver = 'myshili2.mqtt.iot.gz.baidubce.com';
+websocketport = 8884;
+
+mqttuser = 'myshili2/mydevice02';
+mqttpwd = 'e8rP4+S1qKyTp3Fa00AjIaijyXsMiGbFSAqCaqx9auM=';
+mqttlwTopic = 'myzhuti02';
